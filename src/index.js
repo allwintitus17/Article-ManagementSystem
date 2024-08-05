1ccf23a81477c13e2a2ce878ebb33668e0dd2e50
allwintitus <71762131006@cit.edu.in>
2024-08-05 15:48:51 +0530

    fourth commit

--- src/index.js
+++ src/index.js
@@ -2,29 +2,12 @@ const express = require('express');
 const app= express()
 const port =3000;
 require('./db/mongoose');
-// const User=require('./models/users');
-// const Article=require('./models/article');
-// const jwt=require('jsonwebtoken');
 const userRouter=require('./routers/users');
 const articleRouter=require('./routers/article')
 app.use(express.json());
 app.use(userRouter);
 app.use(articleRouter);
-
-
-
-
 app.listen(port,()=>{
     console.log("The server is running in the port"+port);
 })
 
-// const Article =require('./models/article')
-// const User=require('./models/users')
-
-// const main=async()=>{
-
-//      const user=await User.findById("66ae0ae57549dd2aa0e9481d")
-//      await user.populate('articles').execPopulate()
-//      console.log(user.articles);
-// }
-// main()
